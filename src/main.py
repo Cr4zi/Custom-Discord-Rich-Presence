71ce0398b2977d438475a08afaa6e9ef4016b96f
ori <ori.mizrahi1000@gmail.com>
2021-08-09 13:28:00 +0300

    Created function to get every data except time stamp

--- src/main.py
+++ src/main.py
@@ -11,7 +11,50 @@ class MainWindow(QMainWindow):
         super().__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.id = self.get_id()
+        self.details = self.get_details()
+        self.state = self.get_state()
+        self.LargeKey = self.get_large_key()
+        self.largeText = self.get_large_text()
+        self.SmallKey = self.get_small_key()
+        self.SmallText = self.get_small_text()
+        self.Button1Text = self.get_buttonone_text()
+        self.Button1Url = self.get_buttonone_url()
+        self.Button2Text = self.get_buttontwo_text()
+        self.Button2Url = self.get_buttontwo_url()
+
+    def get_id(self):
+        return int(self.ui.ID_textInput.toPlainText())
+    
+    def get_details(self):
+        return str(self.ui.Details_textInput.toPlainText())
         
+    def get_state(self):
+        return str(self.ui.State_textInput.toPlainText())
+
+    def get_large_key(self):
+        return str(self.ui.keylarge_input.toPlainText())
+
+    def get_large_text(self):
+        return str(self.ui.Textlarge_input.toPlainText())
+
+    def get_small_key(self):
+        return str(self.ui.keylarge_input.toPlainText())
+    
+    def get_small_text(self):
+        return str(self.ui.Textsmall_input.toPlainText())
+
+    def get_buttonone_text(self):
+        return str(self.ui.Textone_input.toPlainText())
+
+    def get_buttonone_url(self):
+        return str(self.ui.Urlone_input.toPlainText())
+
+    def get_buttontwo_text(self):
+        return str(self.ui.Texttwo_input.toPlainText())
+    
+    def get_buttontwo_url(self):
+        return str(self.ui.Urltwo_input.toPlainText())
 
 app = QApplication(sys.argv)
 w = MainWindow()
